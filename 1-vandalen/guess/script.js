1e46a580bbfbf3600a21f08cd71a4c644a957a1b
kevinud2014 <kn222gn@student.lnu.se>
2014-11-06 10:01:50 +0000

    har börjat jobba med min script fil med första uppgiften, började med att skriva en if och else men insåg senare att man var tvungen att ha 2 st else if satset för att ha sista else!

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,14 +2,40 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor( Math.random() * (100-1)+1) + 1; // Här har vi den sats som kommer slumpa fram det hemliga talet!
+	
+	
+	var count = 0;// den här var varieblen kommer att fungera som start för räkning! alltså man börjar på 0 gissningar och den ++ på hela tiden!!
+	
+	
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
+		
+		count++;
+		
+		if (number===secret)//ska här kolla ifall nu personen har gissat det hemliga talet och kommer isåfall skriva ut nedrestående rad! med vad numret var, hur många försök personen hade på sig!"
+		{
+			return [true, "Congratulations the secret number was " + secret +" And you tried " + count +" times to find it."];
+		}
+		else if(number>secret)// ska här kolla ifall det hemliga är mindre än det personen skrev in!
+		{
+			return [false, "Sorry the secret number is smaller!"];//returnerar false för talet är inte RÄTT!!
+		}
+		else if(number<secret) //ska här kolla ifall hemliga är mindre än det inmatade talet kommer då visa personen att det hemliga är större än hans inmatade tal!
+		{
+			return [false, "Sorry the secret number is greater!"];//returnerar false för talet är inte RÄTT!!
+		}
+		else// kommer gå in här när perosnen inte har några gissningar kvar och personen får börja om om denne vill det!
+		{
+			return [false, "The number you entered is out of the allowed limits!"];//returnerar false för talet är inte RÄTT!!
+		}
+		
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
 		// Plats för förändring.
+		
 
 
 		// Returnera exempelvis: 

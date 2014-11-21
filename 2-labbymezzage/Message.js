ee8c12499513046c8490207219410637038eae3f
kevinud2014 <kn222gn@student.lnu.se>
2014-11-21 14:12:04 +0000

    nu är jag en bit på vägen, med hjälp från klasskompisar fastnade några gånger

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -7,30 +7,25 @@
           return message;  
         };
         
-        this.getText = function(){
+        this.setText = function(_text){
           message = _text;  
         };
         this.getDate = function(){
-            return date;                //vet ej om denna är rätt!
+            return date;
         };
-        this.setDate = function(){
-            date = _date
+        this.setDate = function(_date){
+            date = _date;
         };
     }
     
     Message.prototype.toString = function(){
         return this.getText()+" ("+this.getDate()+")";
-    }
+    };
     
     Message.prototype.getHTMLText = function(){
         return this.getText().replace(/[\n\r]/g, "<br/>");
-    }
+    };
     
     Message.prototype.getDateText = function(){
         
-    }
-    
-    
-    
-    
-    window.onload = Message;
+    };
--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -3,22 +3,41 @@
     
     var MessageBoard = {
      
-     message: [],
+    message: [],
+    numberOfMessages: 0,
      
-     init:function(e)
-     {
+    init:function()
+    {
+         var newMessage = document.getElementById("sendButton");
+         newMessage.onclick = MessageBoard.sendMessage;
+    },
     
-    var mess = new Message("TestMessage", new Date());
+    sendMessage: function()
+    {
+        var entry = document.getElementById("textEntry"). value;
+        MessageBoard.message.push(new Message(entry, new Date()));
+        alert(MessageBoard.message[MessageBoard.numberOfMessages]);
+        MessageBoard.numberOfMessages++;
+    }
+    
+};
+    
+    /*    var mess = new Message("TestMessage", new Date());
     //alert(mess);  // den här använder toString för utskrift
-    console.log(mess.getText);
+    console.log(mess.getText());
     //alert(mess.getText());  //Skriver enbart ut texten
     mess.setText("Other Text");
     //alert(mess); //Skriver ut meddelandet med ändrad text
-    console.log(mess.getText);
-    console.log(mess.getDate);
-    }
+    console.log(mess.getText());
+    console.log(mess.getDate());
+    MessageBoard.getMessages();*/
     
-    
-};
+    /*getMessages: function(){
+        MessageBoard.message.push(new Message("Testar om ", new Date()), new Message("allt går o skriva såhär", new Date()), new Message("Verkar gå bra!", new Date()));
+
+        console.log(MessageBoard.message[0]);
+        console.log(MessageBoard.message[2].getText());
+    }*/
+
 
 window.onload = MessageBoard.init;

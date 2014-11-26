50ea14b9a74db91cfe24f85955718feaace0a78b
kevinud2014 <kn222gn@student.lnu.se>
2014-11-26 14:54:00 +0000

    nu har jag börjat med labb 3 memory game och jag har lagt upp lite i js filen som jag skapade, t.ex en ny variabel och några funktioner som jag tror ska finnas med!, även länkat in js och css i html doc, lagt till en provisorisk bakgrund på sidan tänkte att om jag hinner så ska mitt spel ha southpark tema med deras aniskten i spelet typ, om detta godkänns av läraren

---
+++ 3-gameon/memory/script/MemoryGame.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+
+
+
+
+    var startMemory = {  // skapar en variabel som ska symbolisera hela programmet!
+        
+        init:function(){ //skapar en init funktion där jag startar run game funktionen !
+            
+        startMemory.runGame();
+    
+        }
+    },
+    
+    runGame:function(){ // skapar här min run game function som jag har tänkt ska starta hela procesen med spelet från början!
+        
+        
+        
+    };
+    
+    window.onload=startMemory.init();

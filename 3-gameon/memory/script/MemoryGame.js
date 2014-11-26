87b7beddc152bc408849474492628faa3727ca7c
kevinud2014 <kn222gn@student.lnu.se>
2014-11-26 15:29:51 +0000

    lite längre tror jag

--- 3-gameon/memory/script/MemoryGame.js
+++ 3-gameon/memory/script/MemoryGame.js
@@ -6,18 +6,20 @@
 
 
     var startMemory = {  // skapar en variabel som ska symbolisera hela programmet!
+    array:[],
         
         init:function(){ //skapar en init funktion där jag startar run game funktionen !
             
-        startMemory.runGame();
-    
+            startMemory.array = RandomGenerator.getPictureArray(5,5);
+            console.log(startMemory.array);
+            
         }
-    },
-    
-    runGame:function(){ // skapar här min run game function som jag har tänkt ska starta hela procesen med spelet från början!
-        
-        
-        
     };
     
-    window.onload=startMemory.init();
+    //runGame:function(){ // skapar här min run game function som jag har tänkt ska starta hela procesen med spelet från början!
+        
+
+        
+    
+    
+    window.onload = startMemory.init();

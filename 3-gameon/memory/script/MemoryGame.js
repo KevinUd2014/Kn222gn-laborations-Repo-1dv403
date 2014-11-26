c277d5e38ddcbbbe674e15a980f3254da0393e44
kevinud2014 <kn222gn@student.lnu.se>
2014-11-26 15:41:26 +0000

    nu känns det som att jag kommit lite på vägen kan nu se arrayens alla innehållande nummer!

--- 3-gameon/memory/script/MemoryGame.js
+++ 3-gameon/memory/script/MemoryGame.js
@@ -11,10 +11,19 @@
         init:function(){ //skapar en init funktion där jag startar run game funktionen !
             
             startMemory.array = RandomGenerator.getPictureArray(5,5);
+            startMemory.startGame(startMemory.array);
             console.log(startMemory.array);
-            
-        }
-    };
+        
+    },
+    
+    startGame:function(array){
+        
+        var memoryArea = document.getElementById("memoryGame");
+        
+    }
+    
+}; // end of startMemory
+
     
     //runGame:function(){ // skapar här min run game function som jag har tänkt ska starta hela procesen med spelet från början!
         

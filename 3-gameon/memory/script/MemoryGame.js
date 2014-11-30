cce21fb1d48ae44e2d280a07676d6739be101373
kevinud2014 <kn222gn@student.lnu.se>
2014-11-30 23:44:32 +0000

    nu kommit lite längre, enligt github så har jag inte pushat men jag pushade i flera omgångar i skolan igår men tydligen så gick de inte fram för jag kollade innan och det var gamla uppdateringar bara, får hoppas denna går fram!

--- 3-gameon/memory/script/MemoryGame.js
+++ 3-gameon/memory/script/MemoryGame.js
@@ -4,14 +4,18 @@
     
     array:[],
     
+    img:0,
+    aElement:0,
+    
     rows:4,
     columns:4,
-    memoryArea: null,
-    aTab:null,
-    img: null,
-    cell: null,
     lastClicked:null,
+    //clickCount:0,
+    defaultPicture:"Pics/0.png",
     
+    testCount:0,
+    turns:0,
+    newClick:0,
     count: 0,
     round: 0,
         
@@ -44,23 +48,32 @@
                 
                 var aElement = document.createElement("a");                         // skapar ett a element
                 aElement.href = "#";                                                // så att man kan taba igenom sidan!
-                aElement.picture = startMemory.array[startMemory.count];            // aElement variablen är modifierad och enda gången detta kan skapa problem är om man jämnför! picture är en ny variabel i a Element
+                
+                
+                //img.picture = startMemory.array[startMemory.count];
+                            
                 //aElement.className = startMemory.array[startMemory.count];               //lägger in så att för varje ny count så läggs en ny bild in!
                 
                 var img = document.createElement("img");
-                img.src = "Pics/0.png";
+                img.src = startMemory.defaultPicture;
+                img.flipImage = false;
                 img.id = "down";
+                img.newPicture = "Pics/" + startMemory.array[startMemory.count] + ".png";// aElement variablen är modifierad och enda gången detta kan skapa problem är om man jämnför! picture är en ny variabel i a Element
+                startMemory.count+=1;
+                
                 
                 //img.className = startMemory.array[startMemory.count];
                 //startMemory.aElement.onclick = startMemory.click(img, img.className); // hittade på en sida på google! // hade tänkt fel men tänkte med dessa innan!
                 
-                aElement.appendChild(img);                                          // lägger till alla förändringarna på dom olika ställena!!
+                aElement.appendChild(img);                          // lägger till alla förändringarna på dom olika ställena!!
                 tdElement.appendChild(aElement);
                 rows.appendChild(tdElement);
                 
-                aElement.onclick = startMemory.flipImage;                           //anropar flip Image funktionen!
+                img.addEventListener("click", startMemory.flipImage);
                 
-                startMemory.count += 1;
+                //aElement.onclick = startMemory.flipImage;                           //anropar flip Image funktionen!
+                
+                //startMemory.count += 1;
             }
             table.appendChild(rows);
         }
@@ -69,8 +82,61 @@
     
     flipImage:function(){
         
+            startMemory.newClick += 1;
+        
+            this.src = this.newPicture;
+            this.flipImage = true;
+            
+            if (this.flipImage === true)  // om man nu 
+            {
+                //nowFliped.removeEventListener("click", startMemory.flipImage);
+                //nowFliped.onclick = null;
+            }
+                
+            if (startMemory.newClick === 2)
+            {
+                var nowFliped = this;  //this är = bilden om jag inte minns fel!
+                var last = startMemory.lastClicked;
+                startMemory.testCount += 1;  //räknar hur många gånger man vänder på 2 st bilder!!
+                
+                nowFliped.addEventListener("click", startMemory.flipImage);
+                nowFliped.onclick = true;
+                last.addEventListener("click", startMemory.flipImage);
+                last.onclick = true;
+                
+                if (nowFliped.newPicture === last.newPicture)// den här funktionen bestämmer om två bilder är av samma karaktär och gör så att dessa stannar uppvända!
+                {
+                    var lastclicked = last;
+                    nowFliped.removeEventListener("click", startMemory.flipImage);
+                    lastclicked.removeEventListener("click", startMemory.flipImage);
+                    nowFliped.onclick = null;
+                    lastclicked.onclick = null;
+                    startMemory.turns += 1; // räknar paren som i nästa funktion kommer stanna programmet när alla bildpar är vända korrekt!
+                    
+                    if (startMemory.turns === startMemory.array.length / 2) //körs när alla par är uppvända korrekt!
+                    {
+                        var resultboard = document.getElementById("ResaultBoard"); // lägger in en ny div tagg 
+                        resultboard.innerHTML = ("Congratulation you made it in " + startMemory.testCount + " turns, you're a BOZZ!");// med detta som argument.
+                    }
+                }
+                
+                else
+                {
+                    setTimeout(function()
+                    {
+                       last.src = startMemory.defaultPicture;
+                       nowFliped.src = startMemory.defaultPicture;
+                    },800);
+                }
+                    startMemory.newClick = 0;
+            }
+                startMemory.lastClicked = this;
+                    
+    }
+};// end of startMemory
+        
         //var self = this;
-        this.firstChild.src = "Pics/" + this.picture + ".png";                      // anropar firstchild i  a taggen !!
+        /*this.firstChild.src = "Pics/" + this.picture + ".png";                      // anropar firstchild i  a taggen !!
         
         startMemory.round += 1;
         
@@ -79,14 +145,13 @@
             //var
             if(startMemory.lastClicked === this.firstChild.src)
             {
-                alert("Gick");
+                
             }
             startMemory.round = 0;
         }
-        startMemory.lastChild = this.firstChild.src;
+        startMemory.lastChild = this.firstChild.src;*/
         
-    }
-};// end of startMemory
+
     
     //runGame:function(){ // skapar här min run game function som jag har tänkt ska starta hela procesen med spelet från början!
         
